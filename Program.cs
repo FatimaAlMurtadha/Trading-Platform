654ed4cb1c5f8a1f752789188d81c141bfd2ee93
FatimaAlmurtadha <sukaina77150@gmail.com>
2025-09-26 14:35:09 +0200

    fix logning in and registration choices and match that with the User class. In order to make it easy to register an account or to log in

--- Program.cs
+++ Program.cs
@@ -14,18 +14,19 @@
 
 
 
-List<User> users = new List<User>();
-// users.Add(new User("u1", "fatima"));
-
+  List<User> users = new List<User>();
+//users.Add(new User("u1", "fatima"));
 
 List<Item> items = new List<Item>();
 
+List<Trade> trades = new List<Trade>();
+
 Console.WriteLine("------- Trading System -------");
 Console.WriteLine();
-Console.WriteLine("What do you want to do: ");
+Console.WriteLine("To be abel to see all the item you should   ");
 Console.WriteLine();
-Console.WriteLine("1. Register an account. ");
-Console.WriteLine("2. Log in ");
+Console.WriteLine("1. Register an account if you are a new user. ");
+Console.WriteLine("2. Or log in ");
 
 string input = Console.ReadLine();
 
@@ -37,22 +38,30 @@ while (running)
 {
   switch (input)
   {
-    case "1": // regester an account
+    case "1":
+      Console.WriteLine("Enter your email address: ");
+      string useremail = Console.ReadLine();
 
+      Console.WriteLine("Enter a password: ");
+      string password = Console.ReadLine();
+
+      users.Add(new User(useremail, password));
+      Console.WriteLine("Your account have successfully registered.");
+      Console.WriteLine("Enter 2 to log in: ");
+      Console.Clear();
 
       break;
 
     case "2": // Log in
-
       if (active_user == null)
       {
-        Console.Write("Username: ");
-        string useremail = Console.ReadLine();
-
+        Console.Write("User Email: ");
+        useremail = Console.ReadLine();
         Console.Clear();
         Console.Write("Password: ");
-        string password = Console.ReadLine();
-        users.Add(new User(username, password));
+        password = Console.ReadLine();
+
+        // bool found_user = false;
 
         Console.Clear();
         foreach (User user in users)
@@ -60,7 +69,7 @@ while (running)
           if (user.TryLogin(useremail, password))
           {
             active_user = user;
-
+            // found_user = true;
             break;
           }
         }
@@ -84,6 +93,152 @@ while (running)
         Console.WriteLine("9. Log out.");
         Console.WriteLine("f. Close.");
         string user_choice = Console.ReadLine();
+
+        switch (user_choice)
+        {
+          case "1": // Upload an item
+
+
+
+            break;
+
+
+          case "2": // 
+
+
+
+            break;
+
+          case "3":
+
+
+            break;
+
+
+          case "4":
+
+
+
+            break;
+
+          case "5":
+
+
+            break;
+
+
+          case "6":
+
+
+
+            break;
+
+          case "7":
+
+
+            break;
+
+
+          case "8":
+
+
+
+            break;
+
+          case "9": // log out
+
+            break;
+
+          case "f":
+
+            running = false;
+            break;
+
+          default:
+
+            break;
+
+        }
+
+
+
+      }
+
+      break;
+
+     default:
+      running = false;
+
+      break;
+  }
+
+  }
+ 
+
+Console.WriteLine("Enter 2 to log in: ");
+string? input_log_in = Console.ReadLine();
+//
+
+/*while (running)
+{
+  switch (input_log_in)
+  {
+    case "1": // regester an account
+
+      Console.WriteLine("Enter your email address: ");
+      string useremail = Console.ReadLine();
+
+      Console.WriteLine("Enter a password: ");
+      string password = Console.ReadLine();
+
+      users.Add(new User(useremail, password));
+      Console.WriteLine("Your account have successfully registered.");
+      Console.WriteLine("Enter 2 to log in: ");
+      input = Console.ReadLine();
+      break;
+
+    case "2": // Log in
+      if (active_user == null)
+      {
+        Console.Write("User Email: ");
+        useremail = Console.ReadLine();
+        Console.Clear();
+        Console.Write("Password: ");
+        password = Console.ReadLine();
+
+        // bool found_user = false;
+
+        Console.Clear();
+        foreach (User user in users)
+        {
+          if (user.TryLogin(useremail, password))
+          {
+            active_user = user;
+            // found_user = true;
+            break;
+          }
+        }
+      }
+      else
+      {
+        Console.Clear();
+        Console.WriteLine("------- Trading System -------");
+        Console.WriteLine("Welcome to our trade system!");
+        Console.WriteLine();
+        Console.WriteLine("What do you want to do today: ");
+        Console.WriteLine();
+        Console.WriteLine("1. Upload an item to start your trade. ");
+        Console.WriteLine("2. Show other users items");
+        Console.WriteLine("3. Request a trade for other users items. ");
+        Console.WriteLine("4. Show trade requests.");
+        Console.WriteLine("5. Accept a trade requests.");
+        Console.WriteLine("6. Deny a trade requests.");
+        Console.WriteLine("7. Accept a trade requests.");
+        Console.WriteLine("8. Show a completed requests.");
+        Console.WriteLine("9. Log out.");
+        Console.WriteLine("f. Close.");
+        string user_choice = Console.ReadLine();
+
         switch (user_choice)
         {
           case "1": // Upload an item
@@ -134,6 +289,11 @@ while (running)
 
             break;
 
+          case "f":
+
+            running = false;
+            break;
+
           default:
 
             break;
@@ -143,7 +303,6 @@ while (running)
 
 
       }
-
 
       break;
 
@@ -155,12 +314,6 @@ while (running)
 
 
 
-}
-
-
-
-
-
-Console.WriteLine("You do not have an account with us!");
+}*/
 
 Console.WriteLine("The programe is done!");
--- Users.cs
+++ Users.cs
@@ -4,27 +4,21 @@ namespace App;
 
 class User 
 {
-  string? UserName;
   string? UserEmail;
   string? _password;
 
-  public User(string? username,string? useremail, string? password)
+  public User(string? useremail, string? password)
   {
-    UserName = username;
     UserEmail = useremail;
     _password = password;
   }
-  public string GetUserName()
-  {
-    return UserName;
-  }
-  public string? GetUserEmail()
+  public string GetUserEmail()
   {
     return UserEmail;
   }
-  public bool TryLogin(string? username,string? useremail, string? password)
+  public bool TryLogin(string? useremail, string? password)
   {
-    return username==UserName && useremail == UserEmail && password == _password;
+    return useremail == UserEmail && password == _password;
   }
 
 }
